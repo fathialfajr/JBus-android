6765408e3ea27cc61e3e75ab10aa906be89dd732
fathialfajr <fathiazalfajr@gmail.com>
2023-11-17 10:03:58 +0700

    PreCS08 - Selesai

---
+++ app/src/main/java/com/FathiaAlfajrJBusRS/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.FathiaAlfajrJBusRS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    <?xml version-"1.O" encoding-"utf-8"?>
+<LinearLayout
+    xaing:android-"htcp://schemas .android.com/apk/res/android"
+    xmins:app-"http://schemas .android.com/apk/res-aut.o"
+    xmIng :toolg="http://schemas.android.com/tools"
+    android:layout width-"match parent"
+    android:layout_height-"match _parent"
+    tools;context.-".LoginActivity"
+    android:gravity-"certer"
+    android:orientation-"vertical"><!-- Add this line -->
+<!-- Fill the lirear layout with some components-->
+</ LinearLayout)
+}
+
